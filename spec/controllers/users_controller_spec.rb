6116a73560ba4a073c4c74b6c838dbe8eda90ca0
wingtonbrito <wingtonbrito@gmail.com>
2017-05-09 10:07:01 -0600

    Improving speed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,24 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  emails = {}
+  emails1 = {}
+  emails2 = {}
+  timedEmails = {}
+  CAPACITY = 1000000;
+  # Java
+  #  int CAPACITY = 100;
+  #   
+  #       Random rnd = new Random();
+        
+  #       for(int i = 0; i < CAPACITY; i++){
+  #           System.out.println(rnd.nextInt(CAPACITY)); 
+  #       }
+  #INITIALIZE test variables
+  CAPACITY.times do |index|
+    timedEmails[index.to_s] = "test#{index.to_s}@email.com" 
+  end
   emails1 = {"0"=>{"alias"=>"test1@email.com"},
             "1"=>{"alias"=>"test2@email.com"},
            }
   emails2 = {"0"=>{"alias"=>"test1@email.com"},
           "1"=>{"alias"=>"test1@email.com"},
          }
-
+  
   describe 'emails duplicates test' do
     it "it has not duplicates" do
-      return_email = controller.remove_duplicated_email(emails).count
-        expect(return_email).not_to eq(2)
+      return_email = controller.remove_duplicated_email(emails1).count
+      expect(return_email).to eq(2)
     end
 
-    it "should pass it has duplicates and removed them" do
+    it "has duplicates and remove them" do
       return_email = controller.remove_duplicated_email(emails2).count
         expect(return_email).to eq(1)
     end
 
+    # #100k inputs under 1 sec
+    it "should perform under 1 sec for 100k inputs" do
+      start_time = Time.now
+      return_email = controller.remove_duplicated_email(timedEmails).count
+      end_time = Time.now
+      duraction = (end_time - start_time) * 1000
+      expect(duraction).to eq(1)
+    end
   end
 end
