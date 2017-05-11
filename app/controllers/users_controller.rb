a9af9a6b9f62ae94b4f8a6467192cbafc057a6c3
wingtonbrito <wingtonbrito@gmail.com>
2017-05-10 18:08:36 -0600

    Cleaner code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'set'
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -28,7 +29,7 @@ class UsersController < ApplicationController
 
   #TODO: make it private/protected.
   # protected
-  
+  #cleaner code
   def remove_duplicated_email emails
     if emails.blank?
       return
@@ -36,77 +37,10 @@ class UsersController < ApplicationController
     if emails.values.count <= 1
       return
     end
-     # Java:
-      # Set<String> removeDups(String[] emails){
-      #   Set<String> noDupsEmails = new HashSet<>();
-      #   for(String email : emails){
-      #     noDupsEmails.add(email)
-      #   }
-      #   return noDupsEmails;
-      # }
-    #sacrify space for speed big O(n)
-    # Leave the resulting list in the original order (same as the input).
-    #The function should be able to handle 100,000 emails
-    #addresses containing 
-    #50% randomly placed duplicates 
-    #in well under 1 second on a typical modern laptop.
     noDupsEmails = {}
-    iterator = 0
-    email_id = 0
-    #working on the optimization solution
-    emails_sorted = merge_sort emails.values
-    while iterator < emails_sorted.size do
-      noDupsEmails[email_id.to_s] = emails_sorted[iterator]
-      email_id += 1
-      #optimization
-      if emails_sorted[iterator] == emails_sorted[iterator+1]
-        iterator += 2
-      else
-        iterator += 1
-      end
+    emails.each do |k, v|
+      noDupsEmails[v] = 1
     end
-    noDupsEmails
-  end
-  #TODO: improve memory uses
-  def merge_sort emails
-    if emails.size <= 1
-      return emails
-    end
-    merging_sort(emails,0,emails.size-1)
-  end
-
-  def merging_sort emails, lo, hi
-    if lo == hi
-      return [emails[lo]]
-    end
-    #TODO: lo + (hi - lo)/2
-    middle = (hi + lo)/2
-    sort1 = merging_sort emails, lo, middle
-    sort2 = merging_sort emails, middle + 1, hi
-    merge sort1, sort2
-  end
-
-  def merge(sort1, sort2)
-    index1 = 0
-    index2 = 0
-    result = []
-    while index1 < sort1.size && index2 < sort2.length
-      if sort1[index1] < sort2[index2]
-        result[index1 + index2] = sort1[index1]
-        index1 += 1
-      else
-        result[index1 + index2] = sort2[index2]
-        index2 += 1
-      end
-    end
-    while index1 < sort1.size
-      result[index1 + index2] = sort1[index1]
-      index1 += 1
-    end
-    while index2 < sort2.size
-      result[index1 + index2] = sort2[index2]
-      index2 += 1
-    end
-    result
+    noDupsEmails.keys
   end
 end
